5545f41d1ef2d3ac4d39457436a7deeed9e9a1f4
John-WD31 <j.cabs8888@gmail.com>
2024-02-20 10:08:03 +0800

    DEL&CLEAR button logic added

--- src/App.js
+++ src/App.js
@@ -57,6 +57,29 @@ const reducer = (state, { type, payload }) => {
         currentOperand: null,
       };
 
+    case ACTIONS.CLEAR:
+      return {};
+
+    case ACTIONS.DELETE_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          overwrite: false,
+          currentOperand: null,
+        };
+      }
+      if (state.currentOperand == null) return state;
+      if (state.currentOperand.length === 1) {
+        return {
+          ...state,
+          currentOperand: null,
+        };
+      }
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1),
+      };
+
     default:
       return state;
   }
@@ -76,8 +99,21 @@ const App = () => {
         </div>
         <div className="current-operand">{currentOperand}</div>
       </div>
-      <button className="span-two">AC</button>
-      <button>DEL</button>
+      <button
+        className="span-two"
+        onClick={() => {
+          dispatch({ type: ACTIONS.CLEAR });
+        }}
+      >
+        AC
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTIONS.DELETE_DIGIT });
+        }}
+      >
+        DEL
+      </button>
       <OperationButton operation="÷" dispatch={dispatch}></OperationButton>
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
